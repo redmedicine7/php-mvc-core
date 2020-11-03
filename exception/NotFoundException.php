54180fe1094c8980a8e56a51e2998fab5e62f4fa
redmedicine7 <redmedicine7@gmail.com>
2020-11-03 09:47:27 -0500

    Initial commit

---
+++ exception/NotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\core\exception;
+
+
+use Exception;
+
+class NotFoundException extends Exception
+{
+
+
+    protected $message = 'Page Not Found';
+    protected $code = '404';
+}
